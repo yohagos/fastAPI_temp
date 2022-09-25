45eec381c5c20e2b3778e1c35d36f51657e8f187
yohagos <yosef.hagos@googlemail.com>
2022-09-25 16:59:19 +0200

    Exceptions & Status Codes

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import SessionLocal, engine
 from sqlalchemy.orm import Session
@@ -14,9 +14,10 @@ def get_db():
     finally:
         db.close()
 
-@app.post('/blog')
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
+    print(new_blog)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
@@ -27,7 +28,12 @@ def get_All_BLogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}')
-def get_All_BLogs(id, db: Session = Depends(get_db)):
+@app.get('/blog/{id}', status_code=200)
+def get_All_BLogs(id, db: Session = Depends(get_db), response: Response = None):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'detail': f'Blog with id {id} is not available'}
+        # 'raise HTTPException' is the same as the two lines above
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with id {id} is not available')
     return blog
--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from .database import Base
 
 class Blog(Base):
     __tablename__ = "blogs"
-    
+
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     body = Column(String)
