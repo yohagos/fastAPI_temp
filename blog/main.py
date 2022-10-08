fdca2e91c658ebead28518fe809cecad739f9be2
yohagos <yosef.hagos@googlemail.com>
2022-10-08 12:32:30 +0200

    Bug fix for update function

--- blog/main.py
+++ blog/main.py
@@ -29,7 +29,10 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
 
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def deleteBlog(id, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with {id} not found')
+    blog.delete(synchronize_session=False)
     db.commit()
     return {'done'}
 
@@ -37,15 +40,12 @@ def deleteBlog(id, db: Session = Depends(get_db)):
 
 @app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
 def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
-    #db.query(models.Blog).filter(models.Blog.id == id).update({'title':request.title, 'body': request.body})
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-        detail=f'Blog with {id} not found')
-    print('app . put ', blog)
-    blog.update(request)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with {id} not found')
+    blog.update({"title": request.title, "body": request.body}, synchronize_session=False)
     db.commit()
-    return 'updated successfully'
+    return 'updated'
 
 
 
