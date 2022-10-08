fa8cef8a4bdbbf16c85aaa89cd5e74e5536aa29b
yohagos <yosef.hagos@googlemail.com>
2022-10-08 14:14:03 +0200

    hashing password

--- blog/main.py
+++ blog/main.py
@@ -1,10 +1,10 @@
-import email
-from unicodedata import name
 from fastapi import FastAPI, Depends, status, Response, HTTPException
+
 from . import schemas, models
 from .database import SessionLocal, engine
 from sqlalchemy.orm import Session
 from typing import List
+from .hashing import Hash
 
 app = FastAPI()
 
@@ -28,8 +28,6 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
-
-
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def deleteBlog(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
@@ -38,8 +36,6 @@ def deleteBlog(id, db: Session = Depends(get_db)):
     blog.delete(synchronize_session=False)
     db.commit()
     return {'done'}
-
-
 
 @app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
 def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
@@ -50,14 +46,10 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     db.commit()
     return 'updated'
 
-
-
 @app.get('/blog', response_model=List[schemas.ShowBlog])
 def get_All_BLogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
-
-
 
 @app.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog)
 def get_All_BLogs(id, db: Session = Depends(get_db), response: Response = None):
@@ -66,15 +58,14 @@ def get_All_BLogs(id, db: Session = Depends(get_db), response: Response = None):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with id {id} is not available')
     return blog
 
-
-@app.post("/user")
+@app.post("/user", response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
-    new_user = models.User(name=request.name, email=request.email, password=request.password)
+    new_user = models.User(name=request.name, email=request.email, password=Hash.get_password_hashed(request.password))
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
-    
+
 # #
 # # Stopped Video at
 # #
