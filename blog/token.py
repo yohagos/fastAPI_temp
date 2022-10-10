cce3fd4dfe9e9c59dda88a1d1dead06c79d6a4d7
yohagos <yosef.hagos@googlemail.com>
2022-10-10 19:17:07 +0200

    just started with Route behind authentication

--- blog/token.py
+++ blog/token.py
@@ -1,7 +1,10 @@
 from typing import Union
 from datetime import datetime, timedelta
 from jose import jwt, JWTError
+from fastapi import Depends
 
+from oauth2 import oauth2_scheme
+from schemas import TokenData
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
@@ -16,3 +19,13 @@ def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+def verifyToke(credentials_exception, token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
